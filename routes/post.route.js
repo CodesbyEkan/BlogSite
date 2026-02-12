94eec44dafa8d864e0328464ca899a48420e986f
CodesbyEkan <111146515+CodesbyEkan@users.noreply.github.com>
2026-02-12 15:18:24 +0100

    Delete routes/post.route.js

--- routes/post.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllPosts, getPostById } from '../controllers/postController.js';
-
-const router = express.Router();
-
-router.get('/', getAllPosts);  // Route to get all posts
-router.get('/:id', getPostById); // Route to get a specific post by its ID
-
-export default router;
