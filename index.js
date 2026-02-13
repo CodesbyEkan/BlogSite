a7f5920677cd8d94590ae2d3a1a1a9bb83d8e826
AYD-7 <ayodejiaronimo@gmail.com>
2026-02-13 19:23:29 +0100

    tweaked UI

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.use("/", pageRouter);
-
+app.use("/", pageRouter); 
 app.listen(PORT, () =>
   console.log(`Server running on http://127.0.0.1:${PORT}.`),
 );
