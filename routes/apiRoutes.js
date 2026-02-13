9c9e056c5a9094aca8fbc527359e9a0277e35526
CodesbyEkan <111146515+CodesbyEkan@users.noreply.github.com>
2026-02-13 17:07:02 +0100

    Delete apiRoutes.js

--- routes/apiRoutes.js
+++
@@ -1,6 +0,0 @@
-// import express from "express";
-// import { getPostData } from "../controllers/getPostData.js ";
-
-// export const apiRouter = express.Router();
-
-// apiRouter.get("/posts", getPostData);
